d96ad053d984d65b5d0d874e707f368b301fd119
Daniel Firth <dan.firth@codethink.co.uk>
2016-05-06 16:01:03 +0100

    String literals maybe better

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::cmp::Ordering;
 
 struct Chunk {
-    name: String,
-    deps: Vec<String>
+    name: &'static str,
+    deps: Vec<&'static str>
 }
 
 impl PartialEq for Chunk {
@@ -26,9 +26,9 @@ impl PartialOrd for Chunk {
 }
 
 fn main() {
-    let x = Chunk { name : String::from("foo"), deps : vec![String::from("bar"), String::from("baz")] };
-    let y = Chunk { name : String::from("bar"), deps : Vec::new() };
-    let z = Chunk { name : String::from("baz"), deps : Vec::new() };
+    let x = Chunk { name : "foo", deps : vec!["bar", "baz"] };
+    let y = Chunk { name : "bar", deps : Vec::new() };
+    let z = Chunk { name : "baz", deps : Vec::new() };
     println!("Is x a dependency of y: {}", x < y);
     println!("Is y a dependency of x: {}", y < x);
     println!("Is z a dependency of x: {}", z < x);
